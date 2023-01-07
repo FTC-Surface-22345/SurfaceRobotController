be030432424c16c740a71e118daab16ad62d431a
Krish Kalla <116767392+Buddhimaan1026@users.noreply.github.com>
2023-01-07 15:58:29 -0500

    Updated Elevator, Claw, and TeleOp, Autonomous Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TeleOpSurface.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TeleOpSurface.java
@@ -63,30 +63,7 @@ public class TeleOpSurface extends LinearOpMode {
                         clawState = false;
                     }
 
-//                    while(gamepad1.right_bumper){
-//                        int state = 0;
-//                        switch (state){
-//                            case 0:
-//                                mainClaw.openServo();
-//                                telemetry.addData("Claw: ", "Open");
-//                                telemetry.update();
-//                                clawState = true;
-//                                state = 1;
-//                                break;
-//                            case 1:
-//                                mainClaw.closeServo();
-//                                telemetry.update();
-//                                telemetry.addData("Claw: ", "Closed");
-//                                clawState = false;
-//                                state = 0;
-//                                break;
-//                        }
-//
-//                    }
-
-
                 }
-
 
             }
         }
