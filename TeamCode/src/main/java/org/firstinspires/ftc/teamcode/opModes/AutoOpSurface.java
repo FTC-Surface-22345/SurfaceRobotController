71e52358f24e92e8a06844d9120a5962b3a98381
Krish Kalla <116767392+Buddhimaan1026@users.noreply.github.com>
2023-01-13 01:04:35 -0500

    AutoOpSurface.java Updates and Changes Programmed Camera Initialization and Viewport

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/AutoOpSurface.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/AutoOpSurface.java
@@ -13,10 +13,8 @@ import org.firstinspires.ftc.teamcode.subsystems.Claw;
 import org.firstinspires.ftc.teamcode.subsystems.Elevator;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.openftc.easyopencv.*;
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
-import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
