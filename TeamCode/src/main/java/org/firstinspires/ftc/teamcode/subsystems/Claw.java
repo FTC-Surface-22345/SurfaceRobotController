46ace223f6b4b8db97b11617b18507ae79a6005f
Krish Kalla <116767392+Buddhimaan1026@users.noreply.github.com>
2023-01-23 00:54:31 -0500

    Claw Updates to run on new System using FTCDashboard constants as enums to increase simplicity and efficiency

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Claw.java
@@ -3,10 +3,13 @@
 
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import static org.firstinspires.ftc.teamcode.subsystems.dashConstants.closeClawL;
+import static org.firstinspires.ftc.teamcode.subsystems.dashConstants.closeClawR;
+import static org.firstinspires.ftc.teamcode.subsystems.dashConstants.openClawL;
+import static org.firstinspires.ftc.teamcode.subsystems.dashConstants.openClawR;
+
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Claw{
@@ -14,26 +17,37 @@ public class Claw{
 
     Servo leftServo;
     Servo rightServo;
-    double openPositionLeft = 0.55; //While opMode Active --> 0.75
-    double closePositionLeft = 0.80;
-    double openPositionRight = 1.0; //While opMode Active --> 0.8
-    double closePositionRight = 0.75;
+//    double openPositionLeft = 0.55; //While opMode Active --> 0.75
+//    double closePositionLeft = 0.80;
+//    double openPositionRight = 1.0; //While opMode Active --> 0.8
+//    double closePositionRight = 0.75;
 
     public void init(HardwareMap hardwareMap){
         leftServo = hardwareMap.get(Servo.class, "leftClaw");
         rightServo = hardwareMap.get(Servo.class, "rightClaw");
     }
 
-    public void openServo(){
-        leftServo.setPosition(openPositionLeft);
-        rightServo.setPosition(openPositionRight);
+    public void open(){
+        leftServo.setPosition(openClawL);
+        rightServo.setPosition(openClawR);
     }
 
-    public void closeServo(){
-        leftServo.setPosition(closePositionLeft);
-        rightServo.setPosition(closePositionRight);
+    public void close(){
+        leftServo.setPosition(closeClawL);
+        rightServo.setPosition(closeClawR);
     }
 
+
+    public void moveClaw(dashConstants.clawState input){
+        switch (input){
+            case OPEN:
+                open();
+
+            case CLOSE:
+                close();
+
+        }
+    }
 
 
 }
